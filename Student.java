c8964612624ea3cfc9b0ded2dbb6351d060a457c
yogesh verma <98902068+yogi-verma@users.noreply.github.com>
2023-04-30 14:28:17 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+	public String name;
+	public String password;
+	public boolean isPremium;
+	
+	public Student(String name, String password, boolean isPremium) {
+		this.name = name;
+		this.password = password;
+		this.isPremium = isPremium;
+	}
+	
+}
