a9043ef7017fb903d9fa898ce3fb8f8da1fc9012
yogesh verma <98902068+yogi-verma@users.noreply.github.com>
2023-04-30 14:26:04 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+	public String name;
+	public boolean isPremium;
+	int amount;
+	
+	public Book(String name, boolean isPremium, int amount) {
+		this.name = name;
+		this.isPremium = isPremium;
+		this.amount = amount;
+	}
+}
